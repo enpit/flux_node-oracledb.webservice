14f1e00967e5591d50f15fa1e9179d712c4be5c5
Janis Krasemann <janis.krasemann@enpit.de>
2015-10-21 22:38:06 +0200

    Further implement the webservice

--- index.js
+++ index.js
@@ -8,9 +8,19 @@ server.get('/todo/all', function (req, res) {
     // TODO implement getting all todos
 });
 
+server.get('/todo/areAllComplete', function (req, res) {
+    console.log('got a request asking if all todos are complete');
+    // TODO implement
+});
+
 server.post('/todo/:text', function (req, res) {
     console.log('got a request to post a todo with text %s', req.params.text);
-    // TODO implement posting a todo
+    var id = (+new Date() + Math.floor(Math.random() * 999999)).toString(36);
+    var todo = {
+    	id: id,
+    	complete: false,
+    	text: req.params.text
+    };
 });
 
 server.put('/todo/:id/:updates', function (req, res) {
@@ -31,8 +41,20 @@ server.del('/todo/:id', function (req, res) {
 server.del('/todo/allCompleted', function (req, res) {
     console.log('got a request to delete all completed todos');
     // TODO implement deleting all completed todos
+    // for (var id in _todos) {
+    // 	if (_todos[id].complete) {
+    // 		destroy(id);
+    // 	}
+    // }
 });
 
-server.listen(7002, function () {
+server.use(function (req, res, next) {
+  console.log('%s: %s', req.method, req.url);
+  next();
+});
+
+// TODO: Initialize Database
+
+server.listen(9999, function () {
     console.log('%s listening at port %s', server.name, server.url);
 });
