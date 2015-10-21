ccb85d943d62fc56b1fdcfdca11b7210adfefc1e
Janis Krasemann <janis.krasemann@enpit.de>
2015-10-22 01:08:17 +0200

    Work around the cross domain call error by adding localhost:3005 to the allowed origin header

--- index.js
+++ index.js
@@ -4,6 +4,27 @@ var db = require('./db_handler.js');
 var server = restify.createServer();
 
 /* WEBSERVICE IMPLEMENTATION */
+
+// Workaround for "No 'Access-Control-Allow-Origin' header is present on the requested resource"-error
+server.use(function (req, res, next) {
+
+    // Website you wish to allow to connect
+    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3005');
+
+    // Request methods you wish to allow
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+    // Request headers you wish to allow
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+    // Set to true if you need the website to include cookies in the requests sent
+    // to the API (e.g. in case you use sessions)
+    res.setHeader('Access-Control-Allow-Credentials', true);
+
+    // Pass to next layer of middleware
+    next();
+});
+
 server.get('/todo/all', function (req, res) {
 
     db.getAll(function (err, result) {
